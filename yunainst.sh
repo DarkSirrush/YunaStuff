6322bec1cd3208f6d56b1f7197af8ba18f7c4fee
DarkSirrush <79956291+DarkSirrush@users.noreply.github.com>
2024-11-20 15:05:59 -0800

    Update yunainst.sh
    Added Error Checking to Mega link, updated link.

--- yunainst.sh
+++ yunainst.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+{ #Checks if sudo password has been set
+setPassword=$(passwd -S | grep -c "NP")
+
+
+if [ $setPassword -eq 0 ]; then
+  echo "sudo password set, skipping..."
+else
+  echo "Please set a password"
+  echo "Do not lose this password, it's a PITA to reset"
+  passwd
+fi
+}
 
 { #install MegaCMD to allow for direct download of Mega.nz links
 if pacman -Qs megacmd > /dev/null ; then
@@ -21,9 +33,21 @@ for package in "${arr[@]}"
 }
 
 [ -f ~/megacmd-x86_64.pkg.tar.zst ] && rm ~/megacmd-x86_64.pkg.tar.zst #cleanup temp file
-[ ! -f ~/Downloads/YunaMS.rar ] && mega-get https://mega.nz/file/M7s2EZwB#CA7-l-pqf3ZnJBwYCSYtrqUT6WhHAT9uSkdU42ds9CE ~/Downloads #Download YunaMS main file
+
+{
+if ! mega-get https://mega.nz/file/Nj0VBKYL#9lU738YDXSPv4ySjMM2bVI_grSTUR9f0zidValq-YhU ; then
+    echo ""
+    echo -e "Mega link out of date, please manually download from \e[0;34mhttps://yuna.ms/download\e[0m and run this script again."
+    echo ""
+    echo -e "Ping \e[0;35m@DarkSirrush\e[0m on Discord to get the link updated. Alternatively, start a pull request at \e[0;34mhttps://github.com/DarkSirrush/YunaStuff\e[0m"
+    echo ""
+    exit
+  else
+    [ ! -f ~/Downloads/YunaMS.rar ] && mega-get https://mega.nz/file/Nj0VBKYL#9lU738YDXSPv4ySjMM2bVI_grSTUR9f0zidValq-YhU ~/Downloads #Download YunaMS main file
+fi
+}
+
 [ ! -f ~/Downloads/linux-patcher.rar ] && curl https://yuna.ms/linux-patcher -o ~/Downloads/linux-patcher.rar #Download linux-specific files
-sudo pacman -Sy --noconfirm wine winetricks wine-mono wine-gecko flatpak unrar ark #Install dependancies from the Arch Repository - note that you may have some of these by default, they are just included for paranoia reasons
 mkdir -p ~/Games
 ark -b ~/Downloads/YunaMS.rar -o ~/Games/
 rm ~/Games/YunaMS/D3DCompiler_47_cor3.dll
@@ -35,11 +59,34 @@ rm ~/Games/YunaMS/wpfgfx_cor3.dll
 ark -b ~/Downloads/linux-patcher.rar -o ~/Games/YunaMS/
 flatpak install -y --noninteractive flathub com.usebottles.bottles #install bottles from Flathub
 flatpak override --user --filesystem=home com.usebottles.bottles
-. ~/.bashrc
-. ~/.bash_profile
+sleep 5 && echo "Sleeping for 5 seconds"
+. "$BASH" &>/dev/null
 flatpak run com.usebottles.bottles #first run, close manually once done first run process... need to see if I can auto close it?
-flatpak run --command=bottles-cli com.usebottles.bottles new --bottle-name yunams --environment gaming #creates the bottle
-WINEPREFIX=~/.var/app/com.usebottles.bottles/data/bottles/bottles/YunaMS winetricks -q dinput8 dotnetcoredesktop6 #installs Patcher dependencies
-flatpak run --command=bottles-cli com.usebottles.bottles add -b yunams -n YunaMS -p ~/Games/YunaMS/YunaMS.exe #adds launch option for game
-flatpak run --command=bottles-cli com.usebottles.bottles add -b yunams -n Patcher -p ~/Games/YunaMS/Patcher.exe #adds launch option for patcher
-flatpak run --command=bottles-cli com.usebottles.bottles run -b yunams -p Patcher #Patcher first run
+flatpak run --command=bottles-cli com.usebottles.bottles new --bottle-name YunaMS --environment gaming #creates the bottle
+WINEPREFIX=~/.var/app/com.usebottles.bottles/data/bottles/bottles/YunaMS winetricks -q dinput8 dotnetdesktop6 #installs Patcher dependencies
+flatpak run --command=bottles-cli com.usebottles.bottles add -b YunaMS -n YunaMS -p ~/Games/YunaMS/YunaMS.exe #adds launch option for game
+flatpak run --command=bottles-cli com.usebottles.bottles add -b YunaMS -n Patcher -p ~/Games/YunaMS/Patcher.exe #adds launch option for patcher
+flatpak run --command=bottles-cli com.usebottles.bottles run -b YunaMS -p Patcher #Patcher first run
+
+{
+if [ ! -f ~/.local/share/applications/YunaMS.desktop ]; then
+mkdir -p ~/.local/share/applications/
+rm ~/.local/share/applications/YunaMS.desktop
+touch ~/.local/share/applications/YunaMS.desktop
+cat << EOF >> ~/.local/share/applications/YunaMS.desktop
+[Desktop Entry]
+Name=YunaMS
+Exec=flatpak run --command=bottles-cli com.usebottles.bottles run -p YunaMS -b 'YunaMS' -- %u
+Type=Application
+Terminal=false
+Categories=Game;
+Icon=~/Games/YunaMS/icon.png
+Comment=Launch YunaMS using Bottles.
+StartupWMClass=YunaMS
+Actions=Configure;
+[Desktop Action Configure]
+Name=Configure in Bottles
+Exec=flatpak run com.usebottles.bottles -b 'YunaMS'
+EOF
+fi
+}
